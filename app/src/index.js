9459ac9ba0f3fba765282f770047e9cb185734c3
Anashm24 <anashoumaid19@gmail.com>
2024-08-27 13:06:33 +0100

    new features in react

--- app/src/index.js
+++ app/src/index.js
@@ -2,27 +2,45 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-const title = "Onyx Storm";
-const img =
-  "https://images-na.ssl-images-amazon.com/images/I/9101MLPcFTL._AC_UL900_SR900,600_.jpg";
-const author = "Rebecca Yarros";
+const firstBook = {
+  title: "Onyx Storm",
+  img: "https://m.media-amazon.com/images/I/9101MLPcFTL._SY522_.jpg",
+  author: "Rebecca Yarros",
+};
+const secondBook = {
+  title: "It Starts with Us",
+  img: "https://m.media-amazon.com/images/I/71PNGYHykrL._SY522_.jpg",
+  author: "Colleen Hoover ",
+};
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      />
     </section>
   );
 };
 
-const Book = () => {
+const Book = (props) => {
   return (
     <article className="book">
-      <h2>{title}</h2>
-      <img src={img} alt={title} />
-      <h4>{author}</h4>
+      <h2>{props.title}</h2>
+      <img src={props.img} alt={props.title} />
+      <h4>{props.author}</h4>
     </article>
   );
 };
